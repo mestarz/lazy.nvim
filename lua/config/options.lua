5396b5cf8a581550e33e5db88df858f63fc61cb2
xinxia <starysky.yaoding@icloud.com>
2025-03-19 00:23:45 +0800

    个人lazyvim分支

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.o.shell = "/bin/bash"
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.g.lazyvim_check_order = false
